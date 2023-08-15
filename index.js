4e0e29ac7e205181862344c79f4e1ef283800d1c
Philip Bush <philipbush@DESKTOP-V1103VC.local>
2023-08-15 21:14:22 +0300

    another changes

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const button = document.getElementById("id1")
 button.addEventListener("click", () => {
     console.log("click");
     console.log("Changed");
+    console.log("another changes")
 });
